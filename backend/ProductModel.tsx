ff28ff230d24967ea0cb3d93fe430d0095dea087
TimeSentinel <lance.stubblefield@gmail.com>
2025-01-02 05:01:40 -0800

    query work

--- backend/ProductModel.tsx
+++ backend/ProductModel.tsx
@@ -8,6 +8,7 @@
 
 
 const Pool = require("pg").Pool;
+
 const pool = new Pool({
     user: "admin",
     host: "192.168.0.188",
@@ -16,12 +17,45 @@ const pool = new Pool({
     port: 5432,
 });
 
+const querySelect = { //TODO: need to finish queries with db access
+    productsList: "SELECT * FROM Products WHERE deleted=False",
+    productsAdd: "INSERT INTO Products (things go here) VALUES ($1, $2) RETURNING *",
+    productsDelete: "UPDATE Products SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+    productsUpdate: "UPDATE Products SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+    optionsGet: "SELECT * FROM Options WHERE product_id = $1",
+    optionsAdd: "INSERT INTO Options VALUES ($1, $2) RETURNING * ",
+    optionsUpdate: "UPDATE Options Set stuff to be equal WHERE id = $2 RETURNING *",
+    optionsDelete: "DELETE FROM Options WHERE id = $1"
+}
+
+const sendQuery = async (query,body) => {
+    try {
+        return await new Promise(function (resolve, reject) {
+            pool.query(query, (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                if (results && results.rows) {
+                    resolve(results.rows);
+                } else {
+                    reject(new Error("No results found"));
+                }
+            });
+        });
+    } catch (error_1) {
+        console.error(error_1);
+        throw new Error("Internal server error");
+    }
+};
+
+
+
 //get all Products our database
 const getProducts = async () => {
     try {
         return await new Promise(function (resolve, reject) {
             // #################################### PRODUCTS QUERY ####################################
-            pool.query("SELECT * FROM Products", (error, results) => {
+            pool.query(querySelect.productsList, (error, results) => {
                 if (error) {
                     reject(error);
                 }
@@ -41,10 +75,10 @@ const getProducts = async () => {
 //create a new Product record in the databsse
 const createProduct = (body) => {
     return new Promise(function (resolve, reject) {
-        const { name, email } = body;
+        const {name, email} = body;
         // #################################### INSERT QUERY ####################################
         pool.query(
-            "INSERT INTO Products (name, email) VALUES ($1, $2) RETURNING *",
+            querySelect.productsAdd,
             [name, email],
             (error, results) => {
                 if (error) {
@@ -62,12 +96,12 @@ const createProduct = (body) => {
     });
 };
 
-//delete a Product
-const deleteProduct = (id) => {
+//delete an Option
+const deleteOption = (id) => {
     return new Promise(function (resolve, reject) {
         // #################################### DELETE QUERY ####################################
         pool.query(
-            "DELETE FROM Products WHERE id = $1",
+            querySelect.optionsDelete,
             [id],
             (error, results) => {
                 if (error) {
@@ -82,10 +116,10 @@ const deleteProduct = (id) => {
 //update a Product record
 const updateProduct = (id, body) => {
     return new Promise(function (resolve, reject) {
-        const { name, email } = body;
+        const {name, email} = body;
         // #################################### UPDATE QUERY ####################################
         pool.query(
-            "UPDATE Products SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+            querySelect.productsUpdate,
             [name, email, id],
             (error, results) => {
                 if (error) {
@@ -104,5 +138,9 @@ module.exports = {
     getProducts,
     createProduct,
     deleteProduct,
-    updateProduct
+    updateProduct,
+    getOptions,
+    addOption,
+    updateOption,
+    deleteOption
 };
