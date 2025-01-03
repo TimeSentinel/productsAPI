0a086c81d63aabb94117b046ee415266c496b1d3
TimeSentinel <lance.stubblefield@gmail.com>
2025-01-03 08:06:46 -0800

    querie structures, notes

--- backend/ProductModel.tsx
+++ backend/ProductModel.tsx
@@ -6,26 +6,48 @@
 //      3. Edit options (options, items, types)
 //      4. Edit products (Add, Edit, Delete)
 
-/* todo: Queries;
-list products w types IN ONE FIELD (ARRAY?)?
-list options for specific product
-list types for specific product
-add product
- -- add options for product
- -- add types for product
-delete product (mark as deleted)
-update product ID
-add/remove options for product ID
-add/remove types for product ID
-delete option id (and remove from products)
-delete type id (and remove from products)
-add category
-del category (if not used, remove relqated subcategories)
-edit category id
-add/delete/edit subcategory id
+/*
+----------------------------------------------------------------------
+todo: Queries;
+LISTS
+productsList: list products w types IN ONE FIELD (ARRAY?)?
+productOptions: list options for specific productID
+productTypes: list types for specific productID
+optionTypes: list option types from optID
+optionItems: list option items from optID
+optionDefaults: list option defaults from optID
 
+ADDS
+productAdd: add product
+productOptionAdd: add options for product ID
+productTypeAdd: add types for product ID
+optionAdd: create new optID
+addOptionItem: add option item from optID
+typeAdd: create new typeID
+categoryAdd: add category
+subcatAdd:
+
+UPDATES
+productUpdate: update product ID
+optionUpdate: edit optID
+typeUpdate: update new typeID
+categoryUpdate: edit category id
+subcatUpdate:
+
+DELETES
+productDelete: delete product (mark as deleted)
+productTypeRemove: remove types for product ID
+productOptionRemove: remove options for product ID
+optionDelete: delete optID (and remove from products)
+deleteOptionItem: remove option item from optID
+typeDelete: delete typeID (and remove from products)
+categoryDelete: del category (if not used, remove relqated subcategories)
+subcatDelete: delete subcategory id
+
+----------------------------------------------------------------------
 todo: add password hash
 todo: add uuid generator
+todo: dotenv environment variables
  */
 
 const Pool = require("pg").Pool;
@@ -38,27 +60,51 @@ const pool = new Pool({
     port: 5432,
 });
 
-const querySelect = { //TODO: need to finish queries with db access
-    productsList: "SELECT * FROM products.list WHERE productDeleted='n'",
-    productsAdd: "INSERT INTO products.list " +
-        "(productID, productName, productShort, productDesc, productPrice, productImage, productCategory, " +
-        "productSubCategory, productDeleted, productDateAdded) " +
-        "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, 'n', $9) RETURNING *",
-    productsDelete: "UPDATE products.list SET productDeleted = 'y' WHERE productID = $1 RETURNING *",
-    productsUpdate: "UPDATE products.list SET " +
-        "productName = $2, productShort = $3, productDesc = $4, productPrice = $5, productImage = $6," +
-        " productCategory = $7, productSubCategory = $8 WHERE productID = $1 RETURNING *",
-    optionsGet: "SELECT * FROM products.options WHERE fkProduct = $1",
-    optionsAdd: "INSERT INTO products.options (keyID, fkProduct) VALUES ($1, $2) RETURNING * ",
-    optionsUpdate: "UPDATE Options Set stuff to be equal WHERE id = $2 RETURNING *",
-    optionsDelete: "DELETE FROM Options WHERE id = $1",
-    typesGet: "SELECT * FROM products.types WHERE fkProduct = $1",
-    typesAdd: "INSERT INTO types VALUES ($1, $2) RETURNING * ",
-    typesUpdate: "UPDATE types Set stuff to be equal WHERE id = $2 RETURNING *",
-    typesDelete: "DELETE FROM types WHERE id = $1"
+//TODO: need to finish queries with db access
+const querySelect = {
+    list: {
+        productsList: "SELECT * FROM products.list WHERE productDeleted='n'",
+        productOptions: "SELECT * FROM products.options WHERE fkProduct = $1",
+        productTypes: "SELECT * FROM products.types WHERE fkProduct = $1",
+        optionTypes: "",
+        optionDefaults: "",
+        optionItems: ""
+    },
+    add: {
+        productAdd: "INSERT INTO products.list " +
+            "(productID, productName, productShort, productDesc, productPrice, productImage, productCategory, " +
+            "productSubCategory, productDeleted, productDateAdded) " +
+            "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, 'n', $9) RETURNING *",
+        productOptionAdd: "",
+        productTypeAdd: "",
+        optionAdd: "",
+        addOptionItem: "",
+        typeAdd: "INSERT INTO types VALUES ($1, $2) RETURNING * ",
+        categoryAdd: "",
+        subcatAdd: ""
+    },
+    update: {
+        productUpdate: "UPDATE products.list SET " +
+            "productName = $2, productShort = $3, productDesc = $4, productPrice = $5, productImage = $6," +
+            " productCategory = $7, productSubCategory = $8 WHERE productID = $1 RETURNING *",
+        optionUpdate: "",
+        typeUpdate: "UPDATE types Set stuff to be equal WHERE id = $2 RETURNING *",
+        categoryUpdate: "",
+        subcatUpdate: ""
+    },
+    delete: {
+        productDelete: "UPDATE products.list SET productDeleted = 'y' WHERE productID = $1 RETURNING *",
+        productOptionRemove: "",
+        productTypeRemove: "",
+        optionDelete: "",
+        deleteOptionItem: "",
+        typeDelete: "DELETE FROM types WHERE id = $1",
+        categoryDelete: "",
+        subcatDelete: ""
+    }
 }
 
-const sendQuery = async (query,body) => {
+const sendQuery = async (query, body) => {
     try {
         return await new Promise(function (resolve, reject) {
             pool.query(query, (error, results) => {
@@ -77,7 +123,6 @@ const sendQuery = async (query,body) => {
         throw new Error("Internal server error");
     }
 };
-
 
 
 //get all Products our database
@@ -165,12 +210,32 @@ const updateProduct = (id, body) => {
     });
 };
 module.exports = {
-    getProducts,
-    createProduct,
-    deleteProduct,
-    updateProduct,
-    getOptions,
-    addOption,
-    updateOption,
-    deleteOption
+    productsList,
+    productOptions,
+    productTypes,
+    productAdd,
+    productDelete,
+    productUpdate,
+    productOptionAdd,
+    productOptionRemove,
+    productTypeAdd,
+    productTypeRemove,
+    optionAdd,
+    optionUpdate,
+    optionDelete,
+    optionTypes,
+    optionDefaults,
+    optionItems,
+    addOptionItem,
+    deleteOptionItem,
+    optionsGet,
+    typeAdd,
+    typeUpdate,
+    typeDelete,
+    categoryAdd,
+    categoryDelete,
+    categoryUpdate,
+    subcatAdd,
+    subcatUpdate,
+    subcatDelete
 };
